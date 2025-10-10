d42e6b9ca02325a18b1a816e788408ae6915d37d
YArazov <156554298+YArazov@users.noreply.github.com>
2025-10-10 16:23:05 +0800

    Update world.js

--- game/world.js
+++ game/world.js
@@ -1,13 +1,17 @@
 // world.js
 // Manages game world state and entities
+import { RigidBody } from './rigidBody.js';
+import Vector2 from './vector2.js';
 
 export class GameWorld {
     constructor() {
         this.entities = [];
     }
 
-    addEntity(entity) {
-        this.entities.push(entity);
+    createRigidBody(shape, image, color=null, velocity = new Vector2(0, 0), acceleration = new Vector2(0, 0)) {
+        const body = new RigidBody(shape, image, color, velocity, acceleration);
+        this.entities.push(body);
+        return body;
     }
 
     update(dt) {
